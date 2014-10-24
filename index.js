5a38e7de8f1c3a693eb993faf0da79be4421a33b
Jari Aarniala <jari.aarniala@gmail.com>
2014-10-24 18:03:52 +0300

    Read in file for now

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 var request = require('request')
 var midiutils = require('midiutils')
 var midifileparser = require('midi-file-parser')
+var _ = require('lodash')
 
-var requestSettings = {
-  method: 'GET',
-  url: process.argv[2]
-  // Will read as binary
-  //encoding: null
-};
+var midi = midifileparser(require('fs').readFileSync('smb1-Theme.mid', 'binary'))
+console.log(midi.tracks.length)
 
-request(requestSettings, function(error, response, body) {
-  if(error) {
-    throw "Uh oh: " + error;
-  }
-  console.log(midifileparser(body))
+_.each(midi.tracks[1], function(x) {
+  console.log(
+    x.subtype,
+    "Time", x.deltaTime,
+    "Note#", x.noteNumber,
+    "Freq", midiutils.noteNumberToFrequency(x.noteNumber),
+    "Name", midiutils.noteNumberToName(x.noteNumber),
+    "Velocity", x.velocity)
 })
-
-var freq = midiutils.noteNumberToFrequency(69)
-console.log(freq)
