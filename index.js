663fc3d06a94fb294718d045c452dd450a2f46a7
Jari Aarniala <jari.aarniala@gmail.com>
2014-10-24 17:44:20 +0300

    Remove submodule, wrap midi stuff into commonjs module

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var midi = require('midiutils')
+var request = require('request')
+var midiutils = require('midiutils')
+var midifile = require('./midifile')
 
-var freq = midi.noteNumberToFrequency(69)
+request("http://www.midiarchive.co.uk/files/Games/Tetris/Tetris%20-%202a.mid", function(error, response, body) {
+  console.log(midifile.MidiFile(body))
+})
+
+var freq = midiutils.noteNumberToFrequency(69)
 console.log(freq)
