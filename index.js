244ff3fa74407860dd8999f0af009f90f3486258
Jari Aarniala <jari.aarniala@gmail.com>
2014-10-26 20:28:21 +0200

    Procedural daydream

--- index.js
+++ index.js
@@ -4,15 +4,43 @@ var midifileparser = require('midi-file-parser')
 var _ = require('lodash')
 
 var midi = midifileparser(require('fs').readFileSync('smb1-Theme.mid', 'binary'))
-console.log(midi.tracks.length)
-console.log(midi.header.ticksPerBeat)
+console.log("Number of MIDI tracks", midi.tracks.length)
+console.log("Ticks per beat:", midi.header.ticksPerBeat)
 
-_.each(midi.tracks[1], function(x) {
-  console.log(
-    x.subtype,
-    "Time", x.deltaTime,
-    "Note#", x.noteNumber,
-    "Freq", midiutils.noteNumberToFrequency(x.noteNumber),
-    "Name", midiutils.noteNumberToName(x.noteNumber),
-    "Velocity", x.velocity)
+function toNotes(track) {
+  var notes = [];
+  var currentNote;
+  var currentTick = 0;
+
+  _.each(track, function(event) {
+    currentTick += event.deltaTime;
+    if(event.subtype === 'noteOn') {
+      if(currentNote) throw "Note already being played. Polyphony schmolyphony!"
+      currentNote = {
+        startTick: currentTick,
+        noteNumber: event.noteNumber
+      }
+    } else if(event.subtype === 'noteOff') {
+      if(!currentNote) throw "Note off with no current note?!?!"
+      if(currentNote.noteNumber !== event.noteNumber) throw "That's not right"
+      notes.push({
+        length: currentTick - currentNote.startTick,
+        noteNumber: event.noteNumber,
+        frequency: midiutils.noteNumberToFrequency(event.noteNumber),
+        name: midiutils.noteNumberToName(event.noteNumber),
+        velocity: event.velocity
+      })
+      currentNote = null
+    }
+  })
+
+  return notes
+}
+
+var notesPerTrack = _.map(midi.tracks, toNotes)
+_.each(notesPerTrack, function(notes, i) {
+  console.log("Track", i)
+  _.each(notes, function(note) {
+    console.log("Note", note.noteNumber)
+  })
 })
